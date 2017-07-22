da859d55043e9d37cd03a6b0354cdc9c3544ef3e
Tolis Chalkis <tolis.chal@gmail.com>
2017-07-22 18:27:38 +0300

    .gitignore Update

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='PAgeRankImplementation',
+    version='0.1.0',
+    description='Sample implementation of Page Rank Algorithm',
+    long_description=readme,
+    author='Tolis Chal, Pantelispanka',
+    author_email='burnyourpc@gmail.com',
+    url='https://github.com/BurnYourPc',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
